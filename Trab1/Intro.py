b9b431f6a642c32a76b6a561c2aa3e5a7e801c78
Alysson Lucas <alyssonlucasvfsoares@gmail.com>
2026-08-14 11:24:13 -0300

    update
    mudança de nome e centralização da janela

--- Trab1/Intro.py
+++ Trab1/Intro.py
@@ -5,13 +5,13 @@ pygame.init()
 pygame.font.init()
 
 
+x, y = (800-135)/2 , (600-35)/2 #random.randint(0, 500), random.randint(0, 400)
 
 font = font = pygame.font.Font(None, 50)
-Nome = "Seu Nome"
-rect =  (260, 100, 175, 35)
+Nome = "Alysson"
+rect =  (x, y, 135, 35)
 
-random.seed(Nome)
-x, y =  random.randint(0, 500), random.randint(0, 400)
+#random.seed(Nome)
 
 print(y)
 
